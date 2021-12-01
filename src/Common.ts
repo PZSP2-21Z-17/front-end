71d7ef99d6bb3f569b7aca008405fb477fdd5c92
Dawid Sygocki <d.sygocki@stud.elka.pw.edu.pl>
2021-12-01 15:39:21 +0100

    Implement simple PDF generation

---
+++ src/Common.ts
@@ -0,0 +1,5 @@
+export const fetchBytes = async (url: string) => await fetch(url).then((result) => result.arrayBuffer());
+export const fetchText = async (url: string) => await fetch(url).then((result) => result.text());
+
+export const nullOf = <T>(obj: T) => null as unknown as T;
+export const undefinedOf = <T>(obj: T) => undefined as unknown as T;
