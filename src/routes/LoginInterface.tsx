7ef7943ac8c846fe70d58b135b7b939bb4aea364
MortonPL <MortonPL@users.noreply.github.com>
2021-12-04 18:22:12 +0100

    Add state setting helpers, tag list and creation

--- src/routes/LoginInterface.tsx
+++ src/routes/LoginInterface.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-import { refresh } from '../Common'
+import { refresh, addToDict } from '../Common'
 import FetchAPI from '../FetchAPI';
 import ReactSession from '../ReactSession';
 
@@ -12,10 +12,6 @@ export const LoginInterface: FunctionComponent<LoginInterfaceProps> = () => {
     const [form, setForm] = useState({});
 
     let isLogged = ReactSession.checkValue('username');
-
-    const setField = (key: any, value: any) => {
-        setForm({...form, [key]: value})
-    }
 
     const handleLoginSubmit = (event: any) => {
         event.preventDefault();
@@ -33,12 +29,12 @@ export const LoginInterface: FunctionComponent<LoginInterfaceProps> = () => {
         <Form onSubmit={handleLoginSubmit}>
             <Form.Group className="mb-3" controlId="formLoginUsername">
                 <Form.Label>E-mail</Form.Label>
-                <Form.Control type="text" placeholder="Enter e-mail" className="w-25" onChange={e => setField('e_mail', e.target.value)}/>
+                <Form.Control type="text" placeholder="Enter e-mail" className="w-25" onChange={e => addToDict(form, setForm, 'e_mail', e.target.value)}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formLoginPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter password" className="w-25" onChange={e => setField('password', e.target.value)}/>
+                <Form.Control type="password" placeholder="Enter password" className="w-25" onChange={e => addToDict(form, setForm, 'password', e.target.value)}/>
             </Form.Group>
             <Button type="submit" variant="primary" className="mb-3">
                 Log In
