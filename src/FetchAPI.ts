bb8d61b649b2176597fa6ce33a60cdb0033907e8
MortonPL <MortonPL@users.noreply.github.com>
2022-01-18 11:11:37 +0100

    Add patch & delete

--- src/FetchAPI.ts
+++ src/FetchAPI.ts
@@ -1,5 +1,5 @@
 export class FetchAPI {
-    static readonly address = 'http://127.0.0.1:8000/';
+    static readonly address = `${window.location.protocol}//${window.location.host}/rest/`;
 
     static fetchGet = async (route: string) => {
         const url = new URL(route, FetchAPI.address).toString();
@@ -14,6 +14,26 @@ export class FetchAPI {
     static fetchPost = async (route: string, data: any) => {
         const url = new URL(route, FetchAPI.address).toString();
         return fetch(url, {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)}
+        ).then(
+            res => res.json()
+        ).catch(
+            error => console.log(error)
+        );
+    };
+
+    static fetchDelete = async (route: string) => {
+        const url = new URL(route, FetchAPI.address).toString();
+        return fetch(url, {method: 'DELETE'}
+        ).then(
+            res => res.json()
+        ).catch(
+            error => console.log(error)
+        );
+    };
+
+    static fetchPatch = async (route: string, data: any) => {
+        const url = new URL(route, FetchAPI.address).toString();
+        return fetch(url, {method: 'PATCH', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)}
         ).then(
             res => res.json()
         ).catch(
