376b54abf97ad1622904c787fa5c4ec0ffc032ff
Dove6 <Dove6@users.noreply.github.com>
2022-02-15 20:14:47 +0100

    Make PDF rendering code human friendly

--- src/exams/ExamPreview.tsx
+++ src/exams/ExamPreview.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { indexToLetter } from "../Common";
 import Task from "../entities/Task";
+import './ExamPreview.css';
 
 type ExamPreviewProps = {
     title?: string;
@@ -11,70 +12,85 @@ type ExamPreviewProps = {
     disableButtons?: boolean;
 };
 
+type ExamRendererWindow = Window & {
+    isInProgress: {
+        get: () => boolean,
+        set: (newValue: boolean) => void
+    },
+    render: (text: string) => Promise<void>,
+    save: (filename?: string) => Promise<void>
+};
+
 export default function ExamPreview(props: ExamPreviewProps) {
     const iframeRef = useRef<HTMLIFrameElement>(null);
     const [areCorrectAnswersMarked, setAreCorrectAnswersMarked] = useState(false);
-    const [isInProgress, setIsInProgress] = useState(true);
+    const [isRendering, setIsRendering] = useState(true);
+    const [isDownloading, setIsDownloading] = useState(false);
 
-    const generateDocument = () => {
-        if (iframeRef.current == null) return;
-        setIsInProgress(true);
-        const heading = props.title ? `# ${props.title.replaceAll('\n', '')}` : '';
-        const quotedPreface = props.preface ? props.preface.split('\n').map(e => `> ${e}`).join('    \n') : '';
-        const flattenedTasks = props.tasks
-            .map((task, qIndex) => {
-                const answers = task.answers
-                    .map((answer, aIndex) => {
-                        let letter = indexToLetter(aIndex);
-                        if (areCorrectAnswersMarked && answer.isCorrect)
-                            letter = `**(${letter})**`;
-                        return `    ${letter}. ${answer.content}`;
-                    }).join('    \n');
-                return `${qIndex + 1}. ${task.content}    \n${answers}`;
-            })
-            .join('\n\n');
-        const iframeDocument = iframeRef.current.contentWindow!.document;
-        iframeDocument.write(`
-            <script src="https://raw.githack.com/eKoopmans/html2pdf/master/dist/html2pdf.bundle.js"></script>
-            <script>window.texme = { style: 'plain' }</script>
-            <script src="https://cdn.jsdelivr.net/npm/texme@1.0.0"></script>
-            <style>
-                textarea { visibility: hidden; }
-                #loader { display: none; width: 75px; }
-                textarea + #loader { display: block; }
-            </style>
-            <textarea>
-${heading}
+    const isInProgress = isDownloading || isRendering;
 
-${quotedPreface}
+    const markdownHeading = props.title ? `# ${props.title.replaceAll('\n', '')}` : '';
+    const markdownPreface = props.preface ? props.preface.split('\n').map(e => `> ${e}`).join('    \n') : '';
+    const markdownTasks = props.tasks.map((task, qIndex) => {
+        const answers = task.answers
+            .map((answer, aIndex) => {
+                let letter = indexToLetter(aIndex);
+                if (areCorrectAnswersMarked && answer.isCorrect)
+                    letter = `**(${letter})**`;
+                return `    ${letter}. ${answer.content}`;
+            }).join('    \n');
+        return `${qIndex + 1}. ${task.content}    \n${answers}`;
+    });
 
-${flattenedTasks.length ? flattenedTasks : props.defaultContent ?? ''}
-            </textarea>
-            <img id="loader" src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif">
-        `);
-        iframeDocument.close();
-        setTimeout(() => setIsInProgress(false), 500);  // TODO: user a proper debounce function
+    const textareaContent = [
+        markdownHeading,
+        markdownPreface,
+        markdownTasks.length ? markdownTasks.join('\n\n') : props.defaultContent ?? ''
+    ].join('\n\n');
+
+    const getRendererWindow = () => {
+        if (!iframeRef?.current?.contentWindow)
+            return null;
+        return iframeRef.current.contentWindow as ExamRendererWindow;
     };
 
     const downloadDocument = () => {
-        if (iframeRef.current == null) return;
-        (iframeRef.current.contentWindow! as any).eval(`
-            scrollTo(0, 0);
-            html2pdf()
-                .set({ html2canvas: { scale: 8 }, pagebreak: { mode: 'avoid-all' } })
-                .from(document.body.parentNode)
-                .save();
-        `);
+        const saveFunc = getRendererWindow()?.save;
+        if (!saveFunc)
+            return;
+        setIsDownloading(true);
+        saveFunc(props.title?.toLowerCase()).then(() => {
+            setIsDownloading(false);
+        });
     };
 
-    useEffect(generateDocument, [props, areCorrectAnswersMarked]);
+    const renderDocument = () => {
+        const renderFunc = getRendererWindow()?.render;
+        if (!renderFunc)
+            return;
+        setIsRendering(true);
+        renderFunc(textareaContent).then(() => {
+            setIsRendering(false);
+        });
+    };
+
+    useEffect(renderDocument, [iframeRef, textareaContent]);
+
+    const generateDownloadButtonContent = () => {
+        if (isInProgress)
+            return (<>
+                <span className="spinner-border spinner-border-sm"></span>{' '}
+                {isDownloading ? 'Downloading...' : 'Rendering...'}
+            </>);
+        return 'Download PDF';
+    };
 
     const buttonPanel = (
-        <div className="d-flex align-items-center" style={{ position: 'fixed', right: '1.5em', bottom: '0.5em' }}>
-            <Form.Check type="switch" label="Show correct" className="me-2"
+        <div className="d-flex align-items-center button-panel">
+            <Form.Check type="switch" label="Show correct" className="me-2" disabled={isInProgress}
                 value={areCorrectAnswersMarked.toString()} onChange={evt => setAreCorrectAnswersMarked(evt.target.checked)} />
             <Button variant="primary" onClick={downloadDocument} disabled={isInProgress}>
-                Download PDF
+                {generateDownloadButtonContent()}
             </Button>
         </div>
     );
@@ -82,7 +98,8 @@ ${flattenedTasks.length ? flattenedTasks : props.defaultContent ?? ''}
     return (
         <div className="border border-dark border-2" style={{ flexBasis: '50%' }}>
             {props.disableButtons !== true ? buttonPanel : <></>}
-            <iframe title="pdf-preview" ref={iframeRef} className="d-block w-100 h-100"></iframe>
+            <iframe className="d-block w-100 h-100" src="/exam_renderer.html" title="Exam renderer"
+                ref={iframeRef} onLoad={renderDocument}></iframe>
         </div>
     );
 }
